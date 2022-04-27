3534ee515a3c2a1e88bea6458908c2937d6dcafd
MATLAB-DOWNLOAD <104482547+MATLAB-DOWNLOAD@users.noreply.github.com>
2022-04-27 11:15:42 +0530

    jnsiadu

--- Exp7_1.m
+++ Exp7_1.m
@@ -9,14 +9,14 @@ v = x .* u;
 
 subplot(3, 1, 1);
 plot(t, x, 'LineWidth', 2);
-title('Carrier Signal - 2012122');
+title('Carrier Signal');
 
 subplot(3, 1, 2);
 plot(t, u, 'm', 'LineWidth', 2);
-title('Square Pulse - 2012122');
+title('Square Pulse');
 
 subplot(3, 1, 3);
 plot(t, v, 'r', 'LineWidth', 2);
-title({'Amplitude Shift Keying';'ASK Signal - 2012122'});
+title({'Amplitude Shift Keying';'ASK Signal'});
 
 print ('Exp7_1','-dpdf','-fillpage');
