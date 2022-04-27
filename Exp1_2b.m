23b16c156e67fc7f00d857f968f9d5ec98f97f7a
MATLAB-DOWNLOAD <104482547+MATLAB-DOWNLOAD@users.noreply.github.com>
2022-04-27 11:26:05 +0530

    gfuen

--- Exp1_2b.m
+++ Exp1_2b.m
@@ -1,7 +1,7 @@
 t = 0 : 0.01 : 4;
 x = 5 * exp (-0.2 * t);
 plot (t, x);
-title ('2012122');
+title ('e^()');
 xlabel ('Time -> t (ms)');
 ylabel ('x(t) = 5 * exp(-0.2*t)');
 print ('Exp1_2b','-dpdf','-fillpage');
