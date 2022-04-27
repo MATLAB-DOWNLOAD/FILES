215e3eb5e2967c228eb4275883d84cc3c53b04e7
MATLAB-DOWNLOAD <104482547+MATLAB-DOWNLOAD@users.noreply.github.com>
2022-04-27 11:18:55 +0530

    kni

--- Exp5_1a.m
+++ Exp5_1a.m
@@ -5,7 +5,7 @@ dt=0 : Ts : 0.4-Ts; %Signal Duration
 x=cos(2*pi*100*dt) + cos(2*pi*500*dt);
 subplot(2, 1, 1);
 plot(dt, x, 'b');
-title('2012122 - X(t)');
+title('X(t)');
 
 nfft=length(x); %length of time domain signal
 nfft2=2^nextpow2(nfft); %length of signal in power of 2
@@ -13,6 +13,6 @@ ff=fft(x, nfft2);
 fff=ff(1:nfft2/2);
 subplot(2, 1, 2);
 plot(abs(fff), 'r');
-title('2012122 - Fourier Transform');
+title('Fourier Transform');
 
 print ('Exp5_1a','-dpdf','-fillpage');
