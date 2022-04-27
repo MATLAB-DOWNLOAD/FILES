06e832b87579efe8fbcee476c4331d78e63f881c
MATLAB-DOWNLOAD <104482547+MATLAB-DOWNLOAD@users.noreply.github.com>
2022-04-27 11:21:34 +0530

    slvmkc

--- Exp3_1b.m
+++ Exp3_1b.m
@@ -4,13 +4,13 @@ f = 1;
 X = exp(-2i*pi*f*t);
 subplot(2, 1, 1);
 plot(t, X, 'm', 'LineWidth', 3)
-title('Original plot - 2012122');
+title('Original plot');
 xlabel('t'); ylabel('X (t)');
 
 Y=cumtrapz(X);
 subplot(2, 1, 2);
 plot(t, Y, 'g', 'LineWidth', 3);
-title('Integrated plot - 2012122');
+title('Integrated plot');
 xlabel('t'); ylabel('Y (t)');
 
 print ('Exp3_1b','-dpdf','-fillpage');
