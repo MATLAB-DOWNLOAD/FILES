5318614a3350b5fbd6ffa178e9d61fc0b72d65f4
MATLAB-DOWNLOAD <104482547+MATLAB-DOWNLOAD@users.noreply.github.com>
2022-04-27 11:17:05 +0530

    losmdco

--- Exp6_1a.m
+++ Exp6_1a.m
@@ -6,17 +6,17 @@ f2=60;
 x1=sin(2*pi*f1*t); % message signal
 subplot(3,1,1);
 plot(t, x1, 'g', 'LineWidth', 2);
-title("Message Signal - 2012122");
+title("Message Signal");
 
 x2=sin(2*pi*f2*t); % carrier signal
 subplot(3, 1, 2);
 plot(t, x2, 'm', 'LineWidth', 1.5);
-title("Carrier Signal - 2012122");
+title("Carrier Signal");
 
 m=8;
 fms=sin((2*pi*f2*t) + (m.*x1)); % frequency modulated signal
 subplot(3, 1, 3);
 plot(t, fms, 'r', 'LineWidth', 1.5);
-title("Frequency Modulated Signal - 2012122");
+title("Frequency Modulated Signal");
 
 print ('Exp6_1a','-dpdf','-fillpage');
