a109835e0a19991d987aad40059a52cb8ac1bda2
MATLAB-DOWNLOAD <104482547+MATLAB-DOWNLOAD@users.noreply.github.com>
2022-04-27 11:26:32 +0530

    skfiewrf

--- Exp1_2a.m
+++ Exp1_2a.m
@@ -1,7 +1,7 @@
 t = 0 : 0.01 : 4;
 x = exp (-0.01 * t);
 plot (t, x);
-title ('2012122');
+title ('e^()');
 xlabel ('Time -> t (ms)');
 ylabel ('x(t) = e^(-0.01*t)');
 print ('Exp1_2a','-dpdf','-fillpage');
