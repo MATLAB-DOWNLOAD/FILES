b05d4de7f2dc9d2226c53a8d6db6478c1d0489c3
MATLAB-DOWNLOAD <104482547+MATLAB-DOWNLOAD@users.noreply.github.com>
2022-04-27 11:16:33 +0530

    sfees

--- Exp6_1c.m
+++ Exp6_1c.m
@@ -6,17 +6,17 @@ f2 = 60;
 x1 = sin(2*pi*f1*t); % message signal
 subplot(3,1,1);
 plot(t, x1, 'g', 'LineWidth', 2);
-title("Message Signal - 2012122");
+title("Message Signal");
 
 x2 = sin(2*pi*f2*t); % carrier signal
 subplot(3, 1, 2);
 plot(t, x2, 'm', 'LineWidth', 1.7);
-title("Carrier Signal - 2012122");
+title("Carrier Signal");
 
 m = 40;
 pms=sin(2*pi*f2*t + m.*x1); % phase modulated signal
 subplot(3, 1, 3);
 plot(t, pms, 'r', 'LineWidth', 1.2);
-title("Phase Modulated Signal - 2012122");
+title("Phase Modulated Signal");
 
 print ('Exp6_1c','-dpdf','-fillpage');
